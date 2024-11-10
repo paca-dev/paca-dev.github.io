0b7f0efd50e97e6a1879d7c2fda29c2c6e4e4425
paca-dev <paca.dev06@gmail.com>
2024-11-10 11:35:09 +0100

    Add serviceworker, fix darkmode

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -2,18 +2,10 @@
    jQuery plugin settings and other scripts
    ========================================================================== */
 
+
+
 $(document).ready(function () {
 
-  // Service worker 
-  if ('serviceWorker' in navigator) {
-    window.addEventListener('load', function() {
-        navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
-            console.log('ServiceWorker registration successful with scope: ', registration.scope);
-        }, function(error) {
-            console.log('ServiceWorker registration failed: ', error);
-        });
-    });
-  }
 
   // FitVids init
   $("#main").fitVids();
